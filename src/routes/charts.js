df690f9f73226b34fefe8aef8a9bddbb9d634d14
Your Name <mike@mikelange.com>
2019-07-26 07:29:05 -0400

    initial push

--- src/routes/charts.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-
-router.get('/charts', function(req, res, next) {
-    res.render('charts', { title: 'Express' });
-});
-
-module.exports = router;
